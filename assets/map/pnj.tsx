684971d9d5f82e49a10ccbc78526931393ec6b81
Benjamin Bosco <benjy.bosco13@gmail.com>
2023-11-20 16:32:34 +0100

    ajout du menu (qlq bug d'affichage du menu à corriger)

---
+++ assets/map/pnj.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pnj" tilewidth="32" tileheight="32" spacing="2" tilecount="210" columns="30">
+ <image source="../../../character_asset/pnj.png" width="1024" height="256"/>
+</tileset>
