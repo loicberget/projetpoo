19e6f9f0a7163bbb87c4ece82836dab406aa8911
loic berget <loicberget@gmail.com>
2024-01-03 20:25:10 +0100

    correction bug animation

--- core/src/com/myrpg/game/entities/Entity.java
+++ core/src/com/myrpg/game/entities/Entity.java
@@ -18,9 +18,9 @@ public class Entity {
     private String _entityID;
     private static final int WALK_NB_COLS = 9;
     private static final int WALK_UP_ROW = 8;
-    private static final int WALK_RIGHT_ROW = 9;
+    private static final int WALK_LEFT_ROW = 9;
     private static final int WALK_DOWN_ROW = 10;
-    private static final int WALK_LEFT_ROW = 11;
+    private static final int WALK_RIGHT_ROW = 11;
     protected Vector2 _currentPlayerPosition;
     protected State _state = State.IDLE;
     protected float _frameTime = 0f;
@@ -48,7 +48,7 @@ public class Entity {
         IDLE, WALKING
     }
 
-    public static final int DIRECTION_UP = 0, DIRECTION_RIGHT = 1, DIRECTION_DOWN = 2, DIRECTION_LEFT = 3;
+    public static final int DIRECTION_UP = 0, DIRECTION_LEFT = 1, DIRECTION_DOWN = 2, DIRECTION_RIGHT = 3;
 
     public Entity() {
         initEntity();
@@ -80,10 +80,10 @@ public class Entity {
 
     private void loadAllAnimations() {
         _walkAnimations = new Array<>();
-        _walkAnimations.set(DIRECTION_UP, createAnimationFromTx(_entitySpritePath, WALK_UP_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
-        _walkAnimations.set(DIRECTION_RIGHT, createAnimationFromTx(_entitySpritePath, WALK_RIGHT_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
-        _walkAnimations.set(DIRECTION_DOWN, createAnimationFromTx(_entitySpritePath, WALK_DOWN_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
-        _walkAnimations.set(DIRECTION_LEFT, createAnimationFromTx(_entitySpritePath, WALK_LEFT_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
+        _walkAnimations.insert(DIRECTION_UP, createAnimationFromTx(_entitySpritePath, WALK_UP_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
+        _walkAnimations.insert(DIRECTION_LEFT, createAnimationFromTx(_entitySpritePath, WALK_LEFT_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
+        _walkAnimations.insert(DIRECTION_DOWN, createAnimationFromTx(_entitySpritePath, WALK_DOWN_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
+        _walkAnimations.insert(DIRECTION_RIGHT, createAnimationFromTx(_entitySpritePath, WALK_RIGHT_ROW, WALK_NB_COLS, FRAME_WIDTH, FRAME_HEIGHT));
     }
 
     public void dispose() {
